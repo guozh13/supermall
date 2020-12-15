4a4e2faa1c106216fafc995ef55f500e8b328c25
guo <170189025@qq.com>
2020-12-15 15:16:36 +0800

    commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <nav-bar class="home-nav">
+      <div slot="center">
+        购物车
+      </div>
+    </nav-bar>
+    {{ banners }}
+    {{ recommend }}
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      banners:[],
+      recommend:[]
+    }
+  },
+  components: {
+    NavBar
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommend =res.data.recommend.list
+    })
+  }
+}
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
